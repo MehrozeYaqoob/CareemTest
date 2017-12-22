ef0eb7db8835edfc864d0ac22e0a3b10b8fb14f1
Mehroze Yaqoob <mehrozeyaqub@gmail.com>
2017-12-22 11:47:07 +0500

    Refactored Some Code

---
+++ app/src/test/java/com/careem/careemtest/activity/MainPresenterTest.java
@@ -0,0 +1,18 @@
+package com.careem.careemtest.activity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hp on 12/18/2017.
+ */
+public class MainPresenterTest {
+
+
+    @Test
+    public void gievenANewViewhandleBtnDateFilter_ () throws Exception{
+
+    }
+
+}
